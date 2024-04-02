e9ac8afa70b1b468e2a92b1418ba9be07f0db74b
Edwin Ko <edwin.mk.ko@gmail.com>
2024-04-02 09:26:55 +0000

    implement enumeration tasks

--- filters.rb
+++ filters.rb
@@ -3,14 +3,54 @@
 
 def find(id)
     # Your code Here
+    @candidates.each do |candidate|
+      if candidate[:id] == id
+        return candidate
+      end
+    end
+
+    return nil
   end
   
   def experienced?(candidate)
     # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    array = []
+    candidates.each do | candidate |
+      if experienced?(candidate) && 
+         ruby_or_python?(candidate) && 
+         github_points_more_than_100?(candidate) && 
+         date_applied_last_15_days?(candidate) && 
+         age_over_17?(candidate)
+        array << candidate
+      end
+    end
+
+    return array
   end
   
   # More methods will go below
+
+  def ruby_or_python?(candidate)
+    candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+  end
+
+  def github_points_more_than_100?(candidate) 
+    candidate[:github_points] >= 100
+  end
+
+  def date_applied_last_15_days?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+
+  def age_over_17?(candidate) 
+    candidate[:age] > 17
+  end
+
+  def ordered_by_qualifications(candidates)
+    sorted_candidates = candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]]}
+  end
--- main.rb
+++ main.rb
@@ -6,4 +6,10 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates
+# pp experienced?(@candidates[1])
+# pp find(15)
+pp qualified_candidates(@candidates)
+pp ruby_or_python?(@candidates[2])
+pp date_applied_last_15_days?(@candidates[0])
+pp ordered_by_qualifications(@candidates)
